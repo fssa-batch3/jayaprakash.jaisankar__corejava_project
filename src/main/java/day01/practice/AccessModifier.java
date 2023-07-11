09eb2554b51b369c5b1b310841186c5eaed785be
Jayaprakash Jaisankar <116724255+JAYAPRAKASHJAISANKAR@users.noreply.github.com>
2023-07-11 17:04:15 +0530

    commit

--- src/main/java/day01/practice/AccessModifier.java
+++ src/main/java/day01/practice/AccessModifier.java
@@ -8,11 +8,22 @@ public class AccessModifier {
     private static String d = "d";
 
     public static void main(String[] args) {
-        AccessModifier accessModifier = new AccessModifier(); 
+    	/*
+    	 * 
+    	 * another method
+    	 * 
+    	 * AccessModifier accessModifier = new AccessModifier(); 
+    	 *   System.out.println(accessModifier.a);
+             System.out.println(accessModifier.b);
+             System.out.println(accessModifier.c);
+             System.out.println(accessModifier.d);
+    	 * 
+    	 */
+        
 
-        System.out.println(accessModifier.a);
-        System.out.println(accessModifier.b);
-        System.out.println(accessModifier.c);
-        System.out.println(accessModifier.d);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
---
+++ src/main/java/day02/practice/User.java
@@ -0,0 +1,44 @@
+package day02.practice;
+
+public class User {
+    private int id;
+    private String name;
+    private String password;
+    private String emailId;
+
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Getter and Setter for emailId
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
